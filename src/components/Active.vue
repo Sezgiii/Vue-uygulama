1e78ac0bb41580aa95d517d85b337c70c41561cb
Sezgi Altıntaş <sezgialtintas93@hotmail.com>
2023-05-22 21:36:35 +0300

    vue uygulama ekle

---
+++ src/components/Active.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="{ active: isActive }" @click="isActive = !isActive"> 
+      <span class="material-symbols-outlined" >
+        radio_button_checked
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+      isActive :false
+    }
+  },
+  watch:{
+    isActive(){
+        if(this.isActive == true){
+          document.body.style.background = "black"
+        }
+        if(this.isActive == false){
+          document.body.style.background = "#f2f2f2"
+        }
+    },
+    
+  }
+}
+</script>
+
+<style>
+    span.material-symbols-outlined{
+      display: flex;
+      direction: rtl;
+      margin-right: 50px ;
+      margin-top: 30px;
+      font-size: 50px;
+      cursor: pointer;
+      color: #76dd78;
+
+    }
+    
+    
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class= "main-nav">
+    <router-link to="/">Projects </router-link> 
+    <router-link to="/add">Add New Projects</router-link>
+   
+  </nav>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .main-nav{
+        text-align: center;
+        margin: 40px auto;
+    }
+    .main-nav a {
+        display: inline-block;
+        text-decoration: none;
+        margin: 0 10px;
+        color : #999;
+        font-size: 20px;
+    }
+    a.router-link-active{
+        border-bottom: 2px solid #76dd78;
+        padding-bottom: 4px;
+        color: #76dd78;
+    }
+
+
+
+</style>
